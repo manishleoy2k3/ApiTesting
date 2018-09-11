bbc8979d79d3dd3c45ffc3daf1c3d5e454ffaf3f
Manish Kumar <manishleoy2k3@gmail.com>
2018-09-11 07:55:42 +0530

    Update Framework constant

--- src/main/java/com/api/framework/constants/FrameworkConstants.java
+++ src/main/java/com/api/framework/constants/FrameworkConstants.java
@@ -6,8 +6,16 @@ import com.aventstack.extentreports.ExtentTest;
 
 public class FrameworkConstants {
 
+	public static final String CONFIGFILE = "configuration/configuration.properties";
 	public static HashMap<String, String> GLOBALCONFIG = new HashMap<String, String>();
+	
 	public static String TMP = "";
+	public static String LOGDIR = "logs";
+	public static String TMPDIR = "tmp";
+	public static String EXECUTION_MODE = "";
+	public static final String EXECUTOR_NAME = System.getProperty("user.name");
+	public static ExtentReports EXTENT;
+	public static ExtentReports REPORT;
 	public static ExtentTest TESTCASE;
 	public static ExtentTest REPORTNODE;
 	public static Integer STEP_COUNTER = 0;
